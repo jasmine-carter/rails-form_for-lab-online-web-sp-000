3be3bf5b2d8d614f9164f3ab699ac19a41417921
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-04 19:45:50 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -1,1 +1,10 @@
- class CreateSchoolClasses
+ class CreateSchoolClasses < ActiveRecord::Migration
+    def change
+      create_table :school_classes do |t|
+        t.string :title
+        t.integer :room_number
+      end
+    end
+
+
+ end
