7dddf46d33732e0db422ffbf6bd4ed896021af9e
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-04 20:03:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,8 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def show
